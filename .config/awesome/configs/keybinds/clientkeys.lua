cdbfb996a7ee4688125550d189cbbfb2fbbd4584
Rodrigues Samuel <sam200133@gmail.com>
2021-04-30 16:45:56 +0200

    Added key to change client volume

--- .config/awesome/configs/keybinds/clientkeys.lua
+++ .config/awesome/configs/keybinds/clientkeys.lua
@@ -1,80 +1,34 @@
 local awful = require("awful");
 local gears = require("gears");
+local utils = require("modules.utils");
 
-Clientkeys =
-    gears.table.join(
-    awful.key(
-        { modkey, "Shift"    },
-        "Right",
-        function ()
-            awful.tag.incmwfact( 0.01)
-        end,
-        {
-            description = "Grow Right",
-            group = "window"
-        }
-    ),
-    awful.key(
-        { modkey, "Shift"    },
-        "Left",
-        function ()
-            awful.tag.incmwfact(-0.01)
-        end,
-        {
-            description = "Grow Left",
-            group = "window"
-        }
-    ),
-    awful.key(
-        { modkey, "Shift"    },
-        "Down",
-        function ()
-            awful.client.incwfact( 0.01)
-        end,
-        {
-            description = "Grow Down",
-            group = "window"
-        }
-    ),
-    awful.key(
-        { modkey, "Shift"    },
-        "Up",
-        function ()
-            awful.client.incwfact(-0.01)
-        end,
-        {
-            description = "Grow Up",
-            group = "window"
-        }
-    ),
-    awful.key(
-        {modkey},
-        "f",
-        function(c)
-            c.fullscreen = not c.fullscreen
-            c:raise()
-        end,
-        {
-            description = "toggle fullscreen",
-            group = "window"
-        }
-    ),
-    awful.key(
-        {modkey, "Ctrl"},
-        "q",
-        function(c)
-            c:kill()
-        end,
-        {description = "close", group = "window"}
-    ),
-    awful.key(
-        {modkey},
-        "n",
-        function(c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end,
-        {description = "minimize", group = "window"}
-    )
-)
+Clientkeys = gears.table.join(awful.key({modkey, "Shift"}, "Right", function()
+    awful.tag.incmwfact(0.01)
+end, {description = "Grow client right", group = "Tag Layout"}),
+                              awful.key({modkey, "Shift"}, "Left", function()
+    awful.tag.incmwfact(-0.01)
+end, {description = "Grow client left", group = "Tag Layout"}),
+                              awful.key({modkey, "Shift"}, "Down", function()
+    awful.client.incwfact(0.01)
+end, {description = "Grow client down", group = "Tag Layout"}),
+                              awful.key({modkey, "Shift"}, "Up", function()
+    awful.client.incwfact(-0.01)
+end, {description = "Grow client up", group = "Tag Layout"}),
+                              awful.key({modkey}, "f", function(c)
+    c.fullscreen = not c.fullscreen
+    c:raise()
+end, {description = "Toggle Fullscreen", group = "Client Managment"}),
+                              awful.key({modkey}, "n", function(c)
+    c.minimized = true
+end, {description = "Minimize", group = "Client Managment"}),
+                              awful.key({modkey, "Ctrl"}, "q", function(c)
+    c:kill()
+end, {description = "Close client", group = "Client Managment"}),
+                              awful.key({modkey}, "XF86AudioRaiseVolume",
+                                        function(c)
+    utils.client_volume(c, "+5")
+end, {description = "Increase client volume", group = "Client Managment"}),
+                              awful.key({modkey}, "XF86AudioLowerVolume",
+                                        function(c)
+    utils.client_volume(c, "-5")
+end, {description = "Decrease client volume", group = "Client Managment"}))
